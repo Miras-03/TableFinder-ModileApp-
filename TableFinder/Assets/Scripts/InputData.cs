0a21d97955e669815db53207bf34dedc2a1a7855
Miras-03 <bolatbek.miras@bk.ru>
2023-03-29 18:34:38 +0300

    Update

--- TableFinder/Assets/Scripts/InputData.cs
+++ TableFinder/Assets/Scripts/InputData.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class InputData : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI indicator;
-    private Time startTime;
+    [SerializeField] InputData inputData;
+    private DateTime date;
+    private string dateString;
 
-    public void Time(Time startTime)
+    private void Awake()
     {
-        this.startTime = startTime;
-        indicator.text = $"{startTime}";
+        date = DateTime.Now;
+        dateString = date.ToString("MM-dd");
+    }
+
+    public void OnDateChanged(string dateString)
+    {
+        if (DateTime.TryParse(dateString, out DateTime newDate))
+        {
+            date = newDate;
+            Debug.Log(date);
+        }
+
+        else
+            Debug.LogError("Invalid date format");
     }
 }
